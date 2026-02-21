96d9d6a6c53d43990b4e4ebb69e69b4941311702
Veera Akshay <veeraakshay172@gmail.com>
2026-02-21 13:41:42 +0530

    Configure app for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/weather-application/',
   plugins: [
     react(),
     tailwindcss(),
